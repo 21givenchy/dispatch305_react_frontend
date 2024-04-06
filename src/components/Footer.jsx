0cc7a72e070eea5231964b993cf376b4c9bed62e
21givenchy <georgekarani44@gmail.com>
2024-04-06 14:12:24 +0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,45 +30,22 @@ const {user, login, logout} = useAuth();
         <Row className="footer-nav-row">
           <div className="footer-nav">
 
+            
+            
+
             <Link exact to="/"
-              as={NavLink} className="footer-link">
-              <span onClick={()=>setCurrentLinkHelper("Home")} className={current_link=="Home"?"secondary-color-span":""}>Home</span>
+              as={NavLink}  className="footer-link">
+              <span className="secondary-color-span">Maintenance</span>
             </Link>
             
-            <Link
-              exact={true}
-              to="https://webapp.simplyfleet.app/services/servicetasks"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="footer-link"
-              >
-            Services
-            </Link>
-
-    
-
-            <Link exact to="/create-account"
-              as={NavLink}  className="footer-link">
-              <span onClick={()=>setCurrentLinkHelper("Contratar")} className={current_link=="Contratar"?"secondary-color-span":""}>Contratar</span>
-            </Link>
-
-            <Link to={agreement_download}  target="_blank" className="footer-link">
-              Agreement
-            </Link>
-
-            <Link exact to="/user-account" as={NavLink}  className="footer-link">
-              <span onClick={()=>setCurrentLinkHelper("Cuenta")} className={current_link=="Cuenta"?"secondary-color-span":""}>Cuenta</span>
-            </Link>
-
-            {user?
-              <Link exact to="/logout" as={NavLink}  className="footer-link">
-                <span onClick={()=>setCurrentLinkHelper("Logout")} className={current_link=="Logout"?"secondary-color-span":""}>Logout</span>
-              </Link>
-            :""}
-
             <Link exact to="/"
               as={NavLink}  className="footer-link">
-              <span className="secondary-color-span">786-783-8421</span>
+              <span className="secondary-color-span">Tracking</span>
+            </Link>
+            
+            <Link exact to="/"
+              as={NavLink}  className="footer-link">
+              <span className="secondary-color-span">invoice Generator</span>
             </Link>
 
           </div>
@@ -76,29 +53,13 @@ const {user, login, logout} = useAuth();
 
         <Row className="footer-nav-row">
           <div className="list-of-usa-states-div">
-          <p>24/7 TRUCK DISPATCH ALL 48 STATES</p>
+          <p>2024 Urban Logistics</p>
 
-            <p>
-              AK, AZ, AR, CA, CO, CT, DE, FL, GA, ID, IL, IN, IA, KS, KY, LA,
-              ME, MD, MA, MI, MN, MS, MO, MT, NE, NV, NH, NJ, NM, NY, NC, ND,
-              OH, OK, OR, PA, RI, SC, SD, TN, TX, UT, VT, VA, WA, WV, WI, WY
-            </p>
-
-            <p>Copyright © 2022 Website Design by Aguilera’s Design Inc.</p>
-              <p className="developer-url-text">Developed by Cecilia Fernandez Aguilera</p>
+           
           </div>
         </Row>
 
-        <Row className="contact-us-s-media-row">
-          <div className="contact-us-s-media-div">
-          <a href="https://www.facebook.com/Dispatch305.US" className="contact-us-s-media-a">
-            <img src={facebookIcon} alt="Facebook Icon Truck Cargo" />
-          </a>
-          <a href="https://www.instagram.com/explore/locations/103686632282068/dispatch305/" className="contact-us-s-media-a">
-            <img src={instagramIcon} alt="Instagram Icon Truck Cargo" />
-          </a>
-          </div>
-        </Row>
+        
 
         <Row className="footer-space-row">
           <div className="footer-space-div" />
