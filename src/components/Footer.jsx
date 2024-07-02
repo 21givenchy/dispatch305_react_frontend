00e6e6b4c90f100da9d4502d934be95771ef7d01
Animaliaurban <102315131+Animaliaurban@users.noreply.github.com>
2024-07-02 15:56:48 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -43,7 +43,7 @@ const {user, login, logout} = useAuth();
               <span className="secondary-color-span">Tracking</span>
             </Link>
             
-            <Link exact to="/"
+            <Link exact to="https://invoice-generator.com/thanks?invoice=1"
               as={NavLink}  className="footer-link">
               <span className="secondary-color-span">invoice Generator</span>
             </Link>
